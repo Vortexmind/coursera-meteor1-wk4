61aaeb0e3b23a929b11c54bd5e1ead33b11e575e
Paolo Tagliaferri <vortexmind@gmail.com>
2015-12-07 23:58:58 +0000

    Whitespace. Add table layout

--- client/main.js
+++ client/main.js
@@ -1,121 +1,126 @@
+Router.route('/', function () {
+	this.layout('ApplicationLayout');
+	this.render('landing_page_content', { to : 'maincontent'});
+});
 
-	Router.route('/', function () {
-		this.layout('ApplicationLayout');
-		this.render('landing_page_content', { to : 'maincontent'});
+Router.route('/website/:_id', function(data) {
+	this.layout('ApplicationLayout', {
+		data: function () { return Websites.findOne({_id: this.params._id}) }
 	});
+	this.render('website_detail', { to : 'maincontent'});
+});
 
-	Router.route('/website/:_id', function(data) {
-		this.layout('ApplicationLayout', {
-			data: function () { return Websites.findOne({_id: this.params._id}) }
+Accounts.ui.config({
+	passwordSignupFields: "USERNAME_AND_EMAIL"
+});
+
+
+/////
+// template helpers
+/////
+
+// helper function that returns all available websites
+Template.website_list.helpers({
+	websites:function(){
+		return Websites.find({},{sort: {votes : -1}});
+	}
+});
+
+// Pretty dates using momentjs
+Template.registerHelper('formatDate', function(date) {
+	return moment(date).startOf('second').fromNow();
+});
+
+
+/////
+// template events
+/////
+
+Template.website_item.events({
+	"click .js-upvote":function(event){
+		// example of how you can access the id for the website in the database
+		// (this is the data context for the template)
+		var website_id = this._id;
+		console.log("Up voting website with id "+website_id);
+
+		Websites.update({_id : website_id },{
+			$inc :  { 'votes' : 1 }
 		});
-		this.render('website_detail', { to : 'maincontent'});
-	});
 
-	Accounts.ui.config({
-		passwordSignupFields: "USERNAME_AND_EMAIL"
-	});
+		return false;// prevent the button from reloading the page
+	},
+	"click .js-downvote":function(event){
 
+		// example of how you can access the id for the website in the database
+		// (this is the data context for the template)
+		var website_id = this._id;
+		console.log("Down voting website with id "+website_id);
 
-	/////
-	// template helpers 
-	/////
+		Websites.update({_id : website_id },{
+			$inc : { 'votes' : -1 }
+		});
 
-	// helper function that returns all available websites
-	Template.website_list.helpers({
-		websites:function(){
-			return Websites.find({},{sort: {votes : -1}});
-		}
-	});
+		return false;// prevent the button from reloading the page
+	}
+})
 
-	// Pretty dates using momentjs
-	Template.registerHelper('formatDate', function(date) {
-		return moment(date).startOf('second').fromNow();
-	});
+Template.website_form.helpers({
+	"getInfoMsg":function() {
+		return Session.get('infoMsg');
+	}
+});
 
+Template.website_form.events({
+	"click .js-toggle-website-form":function(event){
+		$("#website_form").toggle('slow');
+	},
+	"submit .js-save-website-form":function(event){
 
-	/////
-	// template events 
-	/////
+		// here is an example of how to get the url out of the form:
+		var url = event.target.url.value;
+		console.log("The url they entered is: "+url);
 
-	Template.website_item.events({
-		"click .js-upvote":function(event){
-			// example of how you can access the id for the website in the database
-			// (this is the data context for the template)
-			var website_id = this._id;
-			console.log("Up voting website with id "+website_id);
+		Meteor.call("scrapeSite",url,function(err,result) {
 
-			Websites.update({_id : website_id },{
-				$inc :  { 'votes' : 1 }
-			});
-			
-			return false;// prevent the button from reloading the page
-		}, 
-		"click .js-downvote":function(event){
+			if(!err) {
+				// Pull title and description from the scraped content
+				// or populate with 'N/A'
+				var title = result.title || 'N/A';
+				var description = result.description || 'N/A';
 
-			// example of how you can access the id for the website in the database
-			// (this is the data context for the template)
-			var website_id = this._id;
-			console.log("Down voting website with id "+website_id);
+				// Add site to collection
+				// Validation is done server side
+				Websites.insert({
+					url:url,
+					title: title,
+					description:description,
+					createdOn:new Date(),
+					votes: 0
+				});
+			} else {
+				console.log("Error inserting site");
+			}
 
-			Websites.update({_id : website_id },{
-				$inc : { 'votes' : -1 }
-			});
+		});
 
-			return false;// prevent the button from reloading the page
-		}
-	})
+		return false;// stop the form submit from reloading the page
+	}
+});
 
-	Template.website_form.events({
-		"click .js-toggle-website-form":function(event){
-			$("#website_form").toggle('slow');
-		}, 
-		"submit .js-save-website-form":function(event){
+Template.website_comment_form.events({
+	"submit .js-add-website-comment":function(event) {
 
-			// here is an example of how to get the url out of the form:
-			var url = event.target.url.value;
-			console.log("The url they entered is: "+url);
+		var comment = event.target.website_comment.value;
+		var website_id = this._id;
 
-			Meteor.call("scrapeSite",url,function(err,result) {
+		Websites.update({_id: website_id }, { $push : { comments : {
+				author : Meteor.user().username,
+				comment : comment,
+				createdOn : new Date()
+			}
+		}});
 
-				if(!err) {
-					// Pull title and description from the scraped content
-					// or populate with 'N/A'
-					var title = result.title || 'N/A';
-					var description = result.description || 'N/A';
-
-					// Add site to collection
-					// Validation is done server side
-					Websites.insert({
-						url:url,
-						title: title,
-						description:description,
-						createdOn:new Date(),
-						votes: 0
-					});
-				} else {
-					console.log("Error inserting site");
-				} 
-			
-			}); 
-
-			return false;// stop the form submit from reloading the page
-		}
-	});
-
-	Template.website_comment_form.events({
-		"submit .js-add-website-comment":function(event) {
-
-			var comment = event.target.website_comment.value;
-			var website_id = this._id;
-
-			Websites.update({_id: website_id }, { $push : { comments : {
-					author : Meteor.user().username,
-					comment : comment,
-					createdOn : new Date()
-				}
-			}});
-
-			event.target.website_comment.value = ''; // clear form value
-			return false;// stop the form submit from reloading the page
-		}
-	});
+		event.target.website_comment.value = ''; // clear form value
+		return false;// stop the form submit from reloading the page
+	}
+});
